489415e79512497b6a68da66d969125c5517adf1
Eduardo Miquilena <emiquilena@aluxion.com>
2023-02-08 20:55:03 -0400

    added endpoint

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hola mundo desde railway!");
 });
+
+app.get("/saludo", (req, res) => {
+  const { nombre } = req.query;
+
+  res.send(`Hola ${nombre}!`);
+});
 // * => process.env.PORT
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Escuchando en el puerto ${PORT}`));
